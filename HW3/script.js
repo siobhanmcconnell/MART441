574835bfe340705992aebdd19a8dea563aea1dae
siobhanmcconnell <sm245348@umconnect.umt.edu>
2026-01-28 15:30:57 -0700

    HW3 upload

---
+++ HW3/script.js
@@ -0,0 +1,14 @@
+//prints website answer in log
+console.log("Some of my favorite websites are Pinterest, Duolingo, and YouTube.");
+
+//prints game answer in log
+console.log("Some of my favorite games are Super Monkey Ball, Overwatch, and Stardew Valley.");
+
+//prints artist answer in log
+console.log("Some of my favorite artists are Rembrandt, who revolutionized the use of light and shadow, Claude Monet, who led the Impressionist Movement, and Utagawa Hiroshige, who influenced Monet and Van Gogh (another one of my favorites!). They've all created some of the most famous images of all time, which lends to their cultural importance. How we choose to visually represent both people, landscapes, and the emotions they evoke is largely influenced by these guys.");
+
+//asks question and defines answer as variable so it can be printed later
+var showName = prompt("What is your favorite TV show?");
+
+//prints variable from prompt in log in a sentence
+console.log("Wow, " + showName +" is my favorite TV show, too!")
